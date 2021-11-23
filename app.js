924ac71be4b2ea853389a92c50623b7fad672c3d
MichelVazirani <michel.vazirani@gmail.com>
2021-11-23 14:51:08 -0500

    Navigation bar style

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = 5000
 // Static Files
 app.use(express.static('public'));
 // Specific folder example
-// app.use('/css', express.static(__dirname + 'public/css'))
+app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
 // app.use('/img', express.static(__dirname + 'public/images'))
 
