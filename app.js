29b509a02c0c25fff3c8e2d31fb1be6cf00d9280
MichelVazirani <michel.vazirani@gmail.com>
2021-11-23 21:54:52 -0500

    Iamakingawwebsite

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.use(express.static('public'));
 // Specific folder example
 app.use('/css', express.static(__dirname + 'public/css'))
 app.use('/js', express.static(__dirname + 'public/js'))
-// app.use('/img', express.static(__dirname + 'public/images'))
+app.use('/img', express.static(__dirname + 'public/img'))
 
 // Set View's
 app.set('views', './views');
